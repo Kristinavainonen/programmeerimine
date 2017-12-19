1133bd37b37c1b253c2b23a28ceae30173660438
Kristinavainonen <34054829+Kristinavainonen@users.noreply.github.com>
2017-12-19 08:58:23 +0200

    Add files via upload

---
+++ j.py
@@ -0,0 +1,8 @@
+#korrutab kahte täisarvu#
+print("Sisesta esimene korrutatav täisarv: ")
+esimene_täisarv = int(input())
+print("Sisesta teine korrutatav täiarv: ")
+teine_täisarv = int(input())
+
+arvude_korrutis = esimene_täisarv * teine_täisarv
+print(arvude_korrutis)
