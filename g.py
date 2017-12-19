be9a2d5628a997a07f9cd849d4bc61e67ea2ca88
Kristinavainonen <34054829+Kristinavainonen@users.noreply.github.com>
2017-12-19 09:05:30 +0200

    Add files via upload

---
+++ g.py
@@ -0,0 +1,8 @@
+#lahutab kahte täisarvu#
+print("Sisesta esimene lahutatav täisarv: ")
+esimene_täisarv = int(input())
+print("Sisesta teine lahutatav täisarv: ")
+teine_täisarv = int(input())
+
+arvude_vahe = esimene_täisarv - teine_täisarv
+print(arvude_vahe)
